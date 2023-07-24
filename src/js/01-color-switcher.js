c01b2d1bf5572bd2433d58729739efa4d0e3bea7
natalikoyani <natalikagami@gmail.com>
2023-07-25 01:07:34 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
 
+stopBtn.setAttribute('disabled', '');
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+    startBtn.setAttribute('disabled', '');
+    stopBtn.removeAttribute('disabled');
+    timerId = setInterval(() => document.body.style.backgroundColor = getRandomHexColor(), 1000);
+}
+
+function onStop(){
+    clearInterval(timerId);
+    stopBtn.setAttribute('disabled', '');
+    startBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
